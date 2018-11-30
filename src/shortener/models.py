cf24a88984d01aaecbc90f86e98f498a9d1795f8
Ken Vilar <kenvilar@gmail.com>
2018-11-30 21:42:54 +0800

    Clean migrations;Add timestamp fields

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class UrlShort(models.Model):
     url = models.CharField(max_length=220)
     shortcode = models.CharField(max_length=15, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.url)
