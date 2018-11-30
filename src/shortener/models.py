988a12876f42d7184daa4f735bf35a45defe6498
Ken Vilar <kenvilar@gmail.com>
2018-11-30 21:12:42 +0800

    Create model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UrlShort(models.Model):
+    url = models.CharField(max_length=220)
+
+    def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
