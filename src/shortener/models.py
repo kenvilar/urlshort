0c1036d62c5f7f6578b8ba9ee0565db8c4a468da
Ken Vilar <kenvilar@gmail.com>
2018-12-01 00:29:26 +0800

    Refresh shortcodes;Create UrlShort Manager

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 from .utils import code_generator, create_shortcode
 
+
 # Create your models here.
+class UrlShortManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs = super(UrlShortManager, self).all(*args, **kwargs)
+        qs = qs.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = UrlShort.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New codes made: {i}".format(i=new_codes)
+
+
 class UrlShort(models.Model):
     url = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=15, unique=True, blank=True)
+    shortcode = models.CharField(max_length=15, default='abc', unique=True, blank=True)
+    active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UrlShortManager()
+    some_random = UrlShortManager()
 
     def save(self, *args, **kwargs):
         if self.shortcode == None or self.shortcode == "":
