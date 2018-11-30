71b811d5a9e69e8cd1a9cbb9c85ca34c177a172c
Ken Vilar <kenvilar@gmail.com>
2018-11-30 22:13:45 +0800

    Short implementation for shortcode generator

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,10 +4,7 @@ from django.db import models
 
 
 def code_generator(size=6, chars='abcdefghijklmnopqrstuvwxyz'):
-    new_code = ''
-    for _ in range(size):
-        new_code += random.choice(chars)
-    return new_code
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 # Create your models here.
