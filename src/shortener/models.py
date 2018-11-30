d712dc74457be8a67ad133ea440bdef3b75444a9
Ken Vilar <kenvilar@gmail.com>
2018-11-30 21:25:50 +0800

    Make shortcode field unique

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class UrlShort(models.Model):
     url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return str(self.url)
