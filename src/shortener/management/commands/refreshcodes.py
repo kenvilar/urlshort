7bfb2af703b4854a8678e10a71bf48c73a9804e5
Ken Vilar <kenvilar@gmail.com>
2018-12-01 01:40:16 +0800

    Remove comments

--- src/shortener/management/commands/refreshcodes.py
+++ src/shortener/management/commands/refreshcodes.py
@@ -5,8 +5,5 @@ from shortener.models import UrlShort
 class Command(BaseCommand):
     help = 'Refreshes all Url Shortener shortcodes'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('poll_id', nargs='+', type=int)
-
     def handle(self, *args, **options):
         return UrlShort.objects.refresh_shortcodes()
