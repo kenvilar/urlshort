4ee2367c5b8c25f2209b4ed564213e062f39204e
Ken Vilar <kenvilar@gmail.com>
2018-11-30 22:12:00 +0800

    Override save method;Implement shortcode generator

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,4 +1,13 @@
+import random
+
 from django.db import models
+
+
+def code_generator(size=6, chars='abcdefghijklmnopqrstuvwxyz'):
+    new_code = ''
+    for _ in range(size):
+        new_code += random.choice(chars)
+    return new_code
 
 
 # Create your models here.
@@ -7,6 +16,10 @@ class UrlShort(models.Model):
     shortcode = models.CharField(max_length=15, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        print('override save')
+        super(UrlShort, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
